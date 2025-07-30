a5f941ae37f5a3bafce043cdf491936d7da69092
oyy <lyra082172@naver.com>
2025-07-30 21:29:45 +0900

    feat: 브랜드&상품 create() 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/brand/Brand.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/brand/Brand.java
@@ -14,4 +14,28 @@ public class Brand extends BaseEntity {
     String description;
     String imageUrl;
 
+    public static Brand create(String name, String description, String imageUrl) {
+        Brand brand = new Brand();
+
+        brand.validate(name, imageUrl);
+
+        brand.name = name;
+        brand.description = description;
+        brand.imageUrl = imageUrl;
+
+        return brand;
+    }
+
+    private void validate(String name, String imageUrl) {
+        if (name == null || name.isBlank())
+            throw new IllegalArgumentException("브랜드명은 필수입니다.");
+        if (!name.matches("^[가-힣a-zA-Z0-9]+$"))
+            throw new IllegalArgumentException("브랜드명은 한글, 영어, 숫자만 허용됩니다.");
+
+        if (imageUrl == null || imageUrl.isBlank())
+            throw new IllegalArgumentException("이미지 URL은 필수입니다.");
+        if (!imageUrl.matches("^https?://.*\\.(jpg|jpeg|png|gif|webp|svg)$"))
+            throw new IllegalArgumentException("유효한 이미지 확장자를 가진 URL이어야 합니다.");
+    }
+
 }
--- apps/commerce-api/src/main/java/com/loopers/domain/product/Product.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/product/Product.java
@@ -26,4 +26,41 @@ public class Product extends BaseEntity {
 
     String status;
 
+
+    public static Product create(Brand brand, String name, String description, String imageUrl, int price, int stock) {
+        Product product = new Product();
+
+        product.validate(brand, name, imageUrl, price, stock);
+
+        product.brand = brand;
+        product.name = name;
+        product.description = description;
+        product.imageUrl = imageUrl;
+        product.price = price;
+        product.stock = stock;
+        product.status = "ACTIVE"; // 기본 상태를 ACTIVE로 설정
+
+        return product;
+    }
+
+    private void validate(Brand brand, String name, String imageUrl, int price, int stock) {
+        if (brand == null) throw new IllegalArgumentException("브랜드는 필수입니다.");
+
+        if (name == null || name.isBlank())
+            throw new IllegalArgumentException("상품명은 필수입니다.");
+        if (!name.matches("^[가-힣a-zA-Z0-9]+$"))
+            throw new IllegalArgumentException("상품명은 한글, 영어, 숫자만 허용됩니다.");
+
+        if (imageUrl == null || imageUrl.isBlank())
+            throw new IllegalArgumentException("이미지 URL은 필수입니다.");
+        if (!imageUrl.matches("^https?://.*\\.(jpg|jpeg|png|gif|webp|svg)$"))
+            throw new IllegalArgumentException("유효한 이미지 확장자를 가진 URL이어야 합니다.");
+
+        if (price <= 0)
+            throw new IllegalArgumentException("가격은 0 초과이어야 합니다.");
+
+        if (stock <= 0)
+            throw new IllegalArgumentException("재고는 0 초과이어야 합니다.");
+    }
+
 }
